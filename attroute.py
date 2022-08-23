5492f426ffc775754a77bef15f718d6e34db593a
derek <dddoyle@colostate.edu>
2022-08-23 18:49:30 -0500

    Formatting, requirements.txt

--- attroute.py
+++ attroute.py
@@ -2,10 +2,12 @@ import bs4
 from bs4 import BeautifulSoup
 import requests
 import time
+
+
 class Router:
     def __init__(self,
-        address: str,
-        expiration: float = 0):
+                 address: str,
+                 expiration: float = 0):
         """
 
         :param address: address of router
@@ -19,10 +21,11 @@ class Router:
         self.cache_last_updated = time.perf_counter()
 
     def get_broadband_ipv4_address(self) -> str:
-        now = time.perf_counter() / 60.# seconds->minutes
+        now = time.perf_counter() / 60.  # seconds->minutes
         if now - self.cache_last_updated > self.expiration:
             self.bbipv4 = self._get_broadband_ipv4_address()
         return self.bbipv4
+
     def _get_broadband_ipv4_address(self) -> str:
         """
         Internal method for making request to router
@@ -33,15 +36,18 @@ class Router:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        def search_bbipv4(tag : bs4.Tag):
+        def search_bbipv4(tag: bs4.Tag):
             if tag.name == 'td':
                 if tag.find_previous_sibling('th', string='Broadband IPv4 Address'):
                     return True
             return False
+
         return soup.find(search_bbipv4).string
+
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('router_address')
     parser.add_argument('--cache_expiration', help='(minutes)', default=0)
@@ -49,4 +55,3 @@ if __name__ == '__main__':
 
     router = Router(args.router_address, args.cache_expiration)
     print(router.get_broadband_ipv4_address())
-
